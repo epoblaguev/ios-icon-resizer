57741cccd0cee849971d267338399116e252fcd1
Egor Poblaguev <egor.poblaguev@gmail.com>
2020-01-18 00:13:15 -0500

    Updated scripts to include proper filenames for outputs

--- convert_png.sh
+++ convert_png.sh
@@ -1,6 +1,7 @@
-sizes=(20 29 40 50 57 58 60 72 76 80 87 100 114 120 144 152 167 180 512 1024) 
+file_sizes=(57 114 29 58 87 20 40 60 48 55 29 58 87 40 80 120 88 50 100 60 120 180 72 144 76 152 167 172 196 1024)
+file_names=(icon icon@2x icon-small icon-small@2x icon-small@3x icon-20 icon-20@2x icon-20@3x icon-24@2x icon-27.5@2x icon-29 icon-29@2x icon-29@3x icon-40 icon-40@2x icon-40@3x icon-44@2x icon-50 icon-50@2x icon-60 icon-60@2x icon-60@3x icon-72 icon-72@2x icon-76 icon-76@2x icon-83.5@2x icon-86@2x icon-98@2x icon-1024)
 
-for size in "${sizes[@]}"
+for idx in "${!file_sizes[@]}"
 do
-    sips -Z $size $1 --out $size.png
+    sips -Z ${file_sizes[$idx]} $1 --out ${file_names[$idx]}.png
 done
--- convert_svg.sh
+++ convert_svg.sh
@@ -1,7 +1,8 @@
-sizes=(20 29 40 50 57 58 60 72 76 80 87 100 114 120 144 152 167 180 512 1024) 
+file_sizes=(57 114 29 58 87 20 40 60 48 55 29 58 87 40 80 120 88 50 100 60 120 180 72 144 76 152 167 172 196 1024)
+file_names=(icon icon@2x icon-small icon-small@2x icon-small@3x icon-20 icon-20@2x icon-20@3x icon-24@2x icon-27.5@2x icon-29 icon-29@2x icon-29@3x icon-40 icon-40@2x icon-40@3x icon-44@2x icon-50 icon-50@2x icon-60 icon-60@2x icon-60@3x icon-72 icon-72@2x icon-76 icon-76@2x icon-83.5@2x icon-86@2x icon-98@2x icon-1024)
 
-for size in "${sizes[@]}"
+for idx in "${!file_sizes[@]}"
 do
-    qlmanage -t -s $size -o . $1
-    mv $1.png $size.png
+    qlmanage -t -s ${file_sizes[$idx]} -o . $1
+    mv $1.png ${file_names[$idx]}.png
 done
